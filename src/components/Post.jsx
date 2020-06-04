575822fedb50aa47da227911d475dbeab2d875a0
geardy47 <muhammadgenta14@gmail.com>
2020-06-05 01:32:58 +0700

    Mapping actual data on post detail

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Post extends React.Component {
-  state = {
-    post: null,
-  };
-  componentDidMount() {
-    // console.log(this.props);
-    // get the post_id from props (automated class based + Route)
-    let id = this.props.match.params.post_id;
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts/' + id)
-      .then((res) => {
-        this.setState({
-          post: res.data,
-        });
-        // console.log(this.state.post);
-      });
-  }
+  // state = {
+  //   post: null,
+  // };
+  // componentDidMount() {
+  //   // console.log(this.props);
+  //   // get the post_id from props (automated class based + Route)
+  //   let id = this.props.match.params.post_id;
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/posts/' + id)
+  //     .then((res) => {
+  //       this.setState({
+  //         post: res.data,
+  //       });
+  //       // console.log(this.state.post);
+  //     });
+  // }
 
   render() {
-    // const { post } = this.state;
-
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className='post'>
-        <h4 className='center'>{this.state.post.title}</h4>
-        <p className='center'>{this.state.post.body}</p>
+        <h4 className='center'>{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className='center'>Loading post...</div>
@@ -35,4 +33,11 @@ class Post extends React.Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find((post) => post.id === id),
+  };
+};
+
+export default connect(mapStateToProps)(Post);
--- src/reducers/rootReducer.jsx
+++ src/reducers/rootReducer.jsx
@@ -1,19 +1,19 @@
 const initState = {
   posts: [
     {
-      id: 1,
+      id: '1',
       title: 'How to get a job',
       body:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, tempora, deleniti, consectetur sed totam illum cupiditate dignissimos ab quas quasi accusantium. Sint error ipsa enim impedit officiis nam suscipit earum?',
     },
     {
-      id: 2,
+      id: '2',
       title: 'How to get a wife',
       body:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, tempora, deleniti, consectetur sed totam illum cupiditate dignissimos ab quas quasi accusantium. Sint error ipsa enim impedit officiis nam suscipit earum?',
     },
     {
-      id: 3,
+      id: '3',
       title: 'Learn React Redux',
       body:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, tempora, deleniti, consectetur sed totam illum cupiditate dignissimos ab quas quasi accusantium. Sint error ipsa enim impedit officiis nam suscipit earum?',
